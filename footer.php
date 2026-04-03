3c23924a9d1b0b35bf61d6cb7b86715bed8081f6
sharadyadav98175 <sharadyadav98175@gmail.com>
2026-04-03 13:48:12 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+?>
+</main>
+<footer class="site-footer">
+  <div class="container">
+    <small>Student Assignment - <?php echo date('Y'); ?></small>
+  </div>
+</footer>
+<script src="<?php echo htmlspecialchars(BASE_URL); ?>assets/js/app.js" defer></script>
+</body>
+</html>
