3fac365399933b8416ff7ab0f9d53c44b930eb44
sharadyadav98175 <sharadyadav98175@gmail.com>
2026-04-03 13:46:42 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+define('DB_HOST', 'mi-linux.wlv.ac.uk');
+define('DB_NAME', 'db2413835');
+define('DB_USER', '2413835');
+define('DB_PASS', '@Balaji12345');
+
+define('SITE_NAME', 'Student Library System');
+define('BASE_URL', 'https://mi-linux.wlv.ac.uk/~2413835/and/');
